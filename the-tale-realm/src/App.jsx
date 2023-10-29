244f286224ce02259489a60e9525147d3aa45c0c
Bibek <ba03021321@ku.edu.np>
2023-10-29 21:18:05 +0545

    fixed errors

--- the-tale-realm/src/App.jsx
+++ the-tale-realm/src/App.jsx
@@ -3,7 +3,6 @@ import LoginPage from "./Login.jsx";
 import HomePage from "./Home.jsx";
 import SignupPage from "./Signup.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'; // If you haven't imported the Bootstrap CSS yet
-import HomePage from "./Home.jsx";
 
 
 
