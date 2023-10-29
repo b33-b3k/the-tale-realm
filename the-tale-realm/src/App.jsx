9e5eaa5d0ae84c2733a945b033325ef249a4d4d8
Bibek <ba03021321@ku.edu.np>
2023-10-29 19:26:15 +0545

    ok

--- the-tale-realm/src/App.jsx
+++ the-tale-realm/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import LoginPage from "./Login.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css'; // If you haven't imported the Bootstrap CSS yet
 import HomePage from "./Home.jsx";
+import TextEditor from './TextEditor';
+
+
 
 
 
 function App() {
   return (
-    <LoginPage />
+    // <LoginPage />
     // <HomePage />
+    <TextEditor />
+
 
 
 
